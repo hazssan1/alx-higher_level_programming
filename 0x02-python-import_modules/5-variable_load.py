ae606869b7b5c2be20424a87b3339fd360002070
hazssan1 <hazssan1@gmail.com>
2023-03-09 12:43:19 -0800

    variable

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -3,7 +3,7 @@
 # Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
-        """Print the value of variable a from variable_load_5."""
-            from variable_load_5 import a
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
 
-                print(a)
+    print(a))
